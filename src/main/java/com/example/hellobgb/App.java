2af115446c2585e3127615b3578fe88b42ed42b4
amiran <azimi6370@gmail.com>
2023-03-26 17:45:22 +0330

    update: App.java

--- src/main/java/com/example/hellobgb/App.java
+++ src/main/java/com/example/hellobgb/App.java
@@ -2,15 +2,12 @@ package com.example.hellobgb;
 
 import com.bgbrowser.extensionframework.BGBApplication;
 import com.bgbrowser.extensionframework.Browser;
-import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 public class App extends BGBApplication {
 
     @Override
     public void initialize(Browser browser) {
-        Platform.runLater(() -> {
-            new Alert(Alert.AlertType.INFORMATION, "hello, this is a bgb extension example").show();
-        });
+        new Alert(Alert.AlertType.INFORMATION, "hello, this is a bgb extension example").show();
     }
 }
