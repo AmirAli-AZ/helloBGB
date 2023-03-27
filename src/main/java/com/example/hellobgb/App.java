fbae7f8d4053b993389352ece6cc2414a02db216
amiran <azimi6370@gmail.com>
2023-03-27 21:10:00 +0330

    updated to the latest update of framework

--- src/main/java/com/example/hellobgb/App.java
+++ src/main/java/com/example/hellobgb/App.java
@@ -4,10 +4,15 @@ import com.bgbrowser.extensionframework.BGBApplication;
 import com.bgbrowser.extensionframework.Browser;
 import javafx.scene.control.Alert;
 
-public class App extends BGBApplication {
+public class App implements BGBApplication {
 
     @Override
     public void initialize(Browser browser) {
         new Alert(Alert.AlertType.INFORMATION, "hello, this is a bgb extension example").show();
+    }
+
+    @Override
+    public void onDestroyed() {
+        System.out.println("extension destroyed");
     }
 }
